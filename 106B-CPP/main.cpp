28dc28c36a235b9faa4d462a7c24e8181d2ce803
bryan0806 <chunwei.liu@gmail.com>
2014-04-11 18:14:27 +0800

    Initial Commit

---
+++ 106B-CPP/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  106B-CPP
+//
+//  Created by mac on 2014/4/11.
+//  Copyright (c) 2014年 mac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
