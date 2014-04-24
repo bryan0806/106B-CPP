26bb2b97c6387a0ec1e40416735a989259f16181
Bryan@iii <chunwei.liu@gmail.com>
2014-04-24 16:33:08 +0800

    I add this new 2013 class HW1 to here.

---
+++ Life/src/life.cpp
@@ -0,0 +1,27 @@
+// This is the CPP file you will edit and turn in.
+// Also remove these comments here and add your own.
+// TODO: remove this comment header
+
+#include <cctype>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include "console.h"
+#include "filelib.h"
+#include "grid.h"
+#include "gwindow.h"
+#include "simpio.h"
+using namespace std;
+
+int main() {
+    setConsoleSize(700, 400);
+
+
+    // TODO: Finish the program!
+
+
+    cout << "Have a nice Life!" << endl;
+    return 0;
+}
