5a7f5cd08a85989e92bd61945f5471cd0e0a95fb
bryan0806 <chunwei.liu@gmail.com>
2014-05-14 23:36:27 +0800

    testing TokenScanner class

--- Ngrams/src/ngrams.cpp
+++ Ngrams/src/ngrams.cpp
@@ -19,6 +19,7 @@
 #include "set.h"
 #include "simpio.h"
 #include "vector.h"
+#include "tokenscanner.h"
 
 using namespace std;
 
@@ -26,6 +27,19 @@ int main() {
     setConsoleSize(750, 450);
     setConsoleFont("Courier New-16");
     setConsoleEcho(true);
+
+    // test for scanner class
+    string words="<html><b><i>CS106 rules!</i></b></html>";
+    TokenScanner scanner(words);
+    scanner.ignoreWhitespace();
+    while(scanner.hasMoreTokens()){
+        string token = scanner.nextToken();
+        cout << token << endl;
+
+    }
+
+
+
 
     // TODO: Finish the program!
 
