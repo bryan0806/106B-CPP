c87b19c5ff2958122c9f992a0ab06d10c2fd52b1
bryan0806 <chunwei.liu@gmail.com>
2014-05-17 17:19:45 +0800

    This is start file of HW2 of 2011 106B stanford class

---
+++ WordLadder/src/wordladder.cpp
@@ -0,0 +1,46 @@
+// This is the CPP file you will edit and turn in.
+// Also remove these comments here and add your own.
+// TODO: remove this comment header
+
+#include <cctype>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include "console.h"
+#include "filelib.h"
+#include "grid.h"
+#include "hashmap.h"
+#include "hashset.h"
+#include "map.h"
+#include "queue.h"
+#include "random.h"
+#include "set.h"
+#include "simpio.h"
+#include "stack.h"
+#include "vector.h"
+
+using namespace std;
+
+int main() {
+    setConsoleSize(750, 450);
+    setConsoleFont("Courier New-16");
+    setConsoleEcho(true);
+
+    cout << "Welcome to CS 106B Word Ladder." << endl;
+    cout << "If you give me two English words, I will transform the" << endl;
+    cout << "first into the second by changing one letter at a time." << endl;
+    cout << endl;
+
+    cout << "Please type two words: ";
+    string word1;
+    string word2;
+    cin >> word1 >> word2;
+
+    // TODO: Finish the program!
+
+
+    cout << "Have a nice day." << endl;
+    return 0;
+}
