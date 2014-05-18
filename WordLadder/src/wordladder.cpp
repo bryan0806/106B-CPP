f9d4b947d1c64c1168bada3958476c45ed346c3d
bryan0806 <chunwei.liu@gmail.com>
2014-05-18 12:24:59 +0800

    finish the mission: 一開始不懂為何作業講的 怎麼可以找出路徑 後來看了 Breadth first search 的說明後 終於知道怎麼找 hint : https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=3&cad=rja&uact=8&ved=0CEIQtwIwAg&url=http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DQRq6p9s8NVg&ei=HjZ4U9WTLsnp8AXH2YG4Cg&usg=AFQjCNFk51MTb4B5eDJD-7SBhWlWFXjM2g&sig2=BJwdLidyg0_HfUQwITjEEw
    http://en.wikipedia.org/wiki/Breadth-first_search
    
    其中 最重要的是 Frankfurt -> Munchen 最短路徑那張圖
    啟示最大！！

--- WordLadder/src/wordladder.cpp
+++ WordLadder/src/wordladder.cpp
@@ -22,10 +22,12 @@
 #include "vector.h"
 
 using namespace std;
+void wordLadder(string w1, string w2);
+string findNewWord(string oldword,Set<string> dic,Set<string> stack);
 
 int main() {
     setConsoleSize(750, 450);
-    setConsoleFont("Courier New-16");
+    setConsoleFont("Courier New-28");
     setConsoleEcho(true);
 
     cout << "Welcome to CS 106B Word Ladder." << endl;
@@ -40,7 +42,95 @@ int main() {
 
     // TODO: Finish the program!
 
+    //cout << "word1 is "<<word1<<endl;
+    //cout << "word2 is " <<word2<<endl;
+
+    wordLadder(word1,word2);
+
 
     cout << "Have a nice day." << endl;
     return 0;
 }
+
+
+void wordLadder(string w1, string w2){
+    Queue<Stack<string> > ladder;
+    Stack<string> wstack;
+    string topWord,dicword,newWord;
+    wstack.push(w1);
+    ladder.enqueue(wstack);
+    //char buffer[10];
+    Set<string> usedword;
+    usedword.add(w1);
+    // put dictionary into set
+    ifstream ifs;
+    openFile(ifs,"dictionary.txt");
+    Set<string> dictionary;
+    while(getline(ifs,dicword)){ // 注意!! 檔案小時要用這樣ifs.getline(buffer,sizeof(buffer))才是對的方法
+        dictionary.add(dicword); // 大字典檔案需用getline(ifs,string)  小檔案要用上面方法
+    }
+
+
+    while(!ladder.isEmpty()){
+
+        //put stack into set
+
+        wstack=ladder.dequeue();
+        cout<<wstack.toString()<<endl;// for test
+        topWord = wstack.peek();
+        cout << "topWord="<<topWord<<endl;// for test
+
+
+        if(topWord==w2){ break;}
+        else if(topWord != ""){ // find next neighbor and push into stack
+            newWord=topWord;
+            while(newWord!=""){ // 這裡需要一個回圈 一次找齊topWord所有的neighbor
+                newWord=findNewWord(topWord,dictionary,usedword);
+                if(newWord!=""){ // 表示 找到了1.在字典內2. 還沒使用過的 字 且還沒找完所有的組合
+                usedword.add(newWord);
+                Stack<string> newStack = wstack; //每找到一個 新字就成立一個new stack 新字放最上面
+                newStack.push(newWord);
+                cout<<"newStack is " << newStack.toString()<<endl;
+                ladder.enqueue(newStack);//每一個分支都是一個stack
+                }
+            }
+        }
+
+
+
+
+
+    }
+    while(!wstack.isEmpty()){
+        cout<<wstack.pop()<<"  ";
+    }
+}
+
+
+// 小心這邊 我原本丟的第一參數是
+string findNewWord(string oldword,Set<string> dic,Set<string> stack){
+    int i;
+    char j;
+    string tempword;
+
+    for(i=0;i<oldword.length();i++){
+        tempword = oldword; // 一開始以為沒用 但這邊是關鍵！！
+        for(j='a';j<='z';j++){
+
+            //cout << "No, can use 'a'+int way" <<endl;
+            //oldword[i]=j;
+            tempword = tempword.replace(i,1,1,j);
+            //cout << tempword << endl;
+            if(dic.contains(tempword)){
+                //cout<< tempword << endl;
+                if(!(stack.contains(tempword))){
+
+                    //cout << tempword << endl;
+                    return tempword;
+                }
+            }
+        }
+    }
+    return ""; //這邊要注意 沒有會有沒有錯誤訊息的錯誤
+}
+
