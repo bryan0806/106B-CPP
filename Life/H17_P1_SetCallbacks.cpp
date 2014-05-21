bc02208c09c1c37d8774030b99dd366b1ee09eec
bryan0806 <chunwei.liu@gmail.com>
2014-05-21 12:38:34 +0800

    Using new name to start

--- Life/H17_P1_SetCallbacks.cpp
+++ Life/H17_P1_SetCallbacks.cpp
@@ -1,1 +1,27 @@
 
+#include <cctype>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include "console.h"
+#include "filelib.h"
+#include "grid.h"
+#include "hashmap.h"
+#include "hashset.h"
+#include "map.h"
+#include "queue.h"
+#include "random.h"
+#include "set.h"
+#include "simpio.h"
+#include "vector.h"
+#include "tokenscanner.h"
+
+using namespace std;
+
+int main(void){
+    cout<<"ok";
+
+    return 0;
+}
