5645ddae09602bf07ac491f311b562a1b5ac3808
Bryan@iii <chunwei.liu@gmail.com>
2014-05-23 19:04:59 +0800

    Solve print out issue by adding << operator overload.... thanks to Joachim Pileborg at stackoverflow http://stackoverflow.com/questions/23807598/stanford-106b-2013-lib-can-

--- Life/H17_P1_SetCallbacks.cpp
+++ Life/H17_P1_SetCallbacks.cpp
@@ -20,8 +20,45 @@
 
 using namespace std;
 
+struct entryT{
+    string firstNAME;
+    string lastName;
+    string phoneNumber;
+    };
+
+bool operator<(const entryT &front,const entryT &back){
+    if(front.lastName < back.lastName)
+        return true;
+    else if(front.lastName == back.lastName  ){
+        if(front.firstNAME < back.firstNAME) return true;
+        else if(front.firstNAME == back.firstNAME) return false;
+        else return false;
+        }else return false;
+}
+
+std::ostream& operator<<(std::ostream& os, const entryT& et){
+    os << et.firstNAME << " " << et.lastName;
+
+}
+
+void PrintAll(entryT stud){
+    cout << stud.firstNAME <<"  "<<stud.lastName << endl;
+    }
+
 int main(void){
-    cout<<"ok";
+    Set<entryT> students;
+
+    entryT A = {"bryan","liu","0988333222"};
+    entryT B = {"peggy","chang","0963333222"};
+    entryT C = {"penny","chang","0910022222"};
+
+    students.add(A);
+    students.add(B);
+    students.add(C);
+
+
+
+    cout << students.toString();
 
     return 0;
-}
+    }
